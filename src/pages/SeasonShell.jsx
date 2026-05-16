1f506bf57debea5504286210ed7380dc15bc333b
Narasimha <pappulanarasimha371@gmail.com>
2026-05-16 14:56:55 +0530

    Removed that create match button

--- src/pages/SeasonShell.jsx
+++ src/pages/SeasonShell.jsx
@@ -1,21 +1,26 @@
-import { Outlet, useNavigate, useParams } from "react-router-dom";
+import { Outlet, useNavigate, useParams, useLocation } from "react-router-dom";
 import AppShell from "../components/AppShell";
 import styles from "../pages/Home.module.css";
 
 export default function SeasonShell() {
   const navigate = useNavigate();
+  const location = useLocation();
   const { seasonId } = useParams();
+
+  const isStatsPage = location.pathname.includes("/stats");
 
   return (
     <AppShell
       title="Gully Cricket"
       bottomAction={
-        <button
-          className={styles.createBtn}
-          onClick={() => navigate(`/season/${seasonId}/create-match`)}
-        >
-          + Create Match
-        </button>
+        !isStatsPage && (
+          <button
+            className={styles.createBtn}
+            onClick={() => navigate(`/season/${seasonId}/create-match`)}
+          >
+            + Create Match
+          </button>
+        )
       }
     >
       <Outlet />
